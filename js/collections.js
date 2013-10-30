57389d3583f90266d234081edd047c83a26d3e04
Byron Hulcher <byron@strayboots.com>
2013-10-29 21:25:18 -0400

    Limitting to 10 results, shows API parameters

--- js/collections.js
+++ js/collections.js
@@ -4,6 +4,13 @@ var EventCollection = Backbone.Collection.extend({
     parse: function(response){
         return response.results
     },
+    pagination : function(perPage, page) {
+       page = page-1;
+       var collection = this;
+       collection = _(collection.rest(perPage*page));
+       collection = _(collection.first(perPage));    
+       return collection;
+    },
     sync: function(method, model, options){  // jsonp hack via http://iainjmitchell.com/blog/?p=777
         options.dataType = "jsonp";  
         return Backbone.sync(method, model, options);  
--- js/views.js
+++ js/views.js
@@ -1,5 +1,6 @@
 var EventView = Backbone.View.extend({
     tagName:  "li",
+    template_el: $("#event-template");
     render: function() {
         var link = $('<a />', {
             id:'event_'+this.model.id,
@@ -7,33 +8,45 @@ var EventView = Backbone.View.extend({
             target: '_blank'
         }).text(this.model.get('name'));
         this.$el.html(link);
+
+        var list = $('<ul />');
+        _.each(this.model.attributes, function(value, attribute){
+            $('<li />').text(attribute + ": " + value).appendTo(list);
+        });
+        this.$el.append(list);
         return this;
-    }
+    },
+
 });
 
 var AppView = Backbone.View.extend({
     el: $("#events"),
     initialize: function(){
+        var self = this;
         this.events = new EventCollection;
-        this.listenTo(this.events, 'add', this.addOne);
-        this.listenTo(this.events, 'reset', this.addAll);
-        this.listenTo(this.events, 'all', this.render);
+        //this.listenTo(this.events, 'add', this.addOne);
+        //this.listenTo(this.events, 'reset', this.addAll);
+        //this.listenTo(this.events, 'all', this.render);
         this.events.fetch({
             data: {
                 'key': MEETUP_API_KEY,
                 'page': 40,
                 'zip': 11226,
             },
-            remove: false
+            remove: false,
+            success: function(collection, data){
+                self.render();
+            }
         });
     },
-    render: function(){},
+    render: function(){
+        var slice = this.events.slice(0,10);
+        _.each(slice, this.addOne);
+    },
     addOne: function(event){
         var view = new EventView({model:event});
         $("#events-list").append(view.render().el);
-
     },
     addAll: function(){
-        this.events.each(this.addOne, this);
     }
 });
